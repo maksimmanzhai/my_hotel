a3c6aca10ae492c59972804a8f8e3a1d4b571ff9
maksimmanzhai <bender23max@gmail.com>
2019-07-01 22:20:51 +0300

    Adding validation to model Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,12 @@
 class Reservation < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :reservation_dates_must_make_sense
+
+  private
+  def reservation_dates_must_make_sense
+    if end_date <= start_date
+      errors.add(:start_date, 'has to be before the end date')
+    end
+  end
 end
